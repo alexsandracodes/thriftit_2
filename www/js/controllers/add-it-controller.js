2cc2ec7ea2bb3a042e8decde2e19922a1b7112b4
alexsandracodes <aguerra14@gmail.com>
2015-05-27 19:00:40 -0800

    this is a commit that has the camera access working from addit page button, but not adding to library. saving this before re-doing camera according to ngCordova

--- www/js/controllers/add-it-controller.js
+++ www/js/controllers/add-it-controller.js
@@ -3,15 +3,22 @@ angular
 .controller("AddItController", function($scope, Camera) {
 
   $scope.getPhoto = function() {
-    Camera.getPicture().then(function(imageURI) {
+    Camera.getPicture().then(
+	//CameraSuccess (see documentation: https://github.com/apache/cordova-plugin-camera)
+     function(imageURI) {
       console.log(imageURI);
       $scope.lastPhoto = imageURI;
-    }, function(err) {
+    },
+    //cameraError
+     function(err) {
       console.err(err);
-    }, {
+    }, 
+	//camera options
+    {
       quality: 75,
       targetWidth: 320,
       targetHeight: 320,
+      sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
       saveToPhotoAlbum: true
     });
   };
